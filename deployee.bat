6a61aa0105fb7ff1536881070e3bd035cd4bb34e
zhubj <810864669@qq.com>
2019-10-08 18:09:31 +0800

    init12

--- deployee.bat
+++ deployee.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Salesforce CLI\bin\sfdx" force:source:deploy -x ./manifest/package2.xml -u mycitestpartial --testlevel RunSpecifiedTests --runtests LWCUploadTPACtlTest
+sfdx force:source:deploy -x ./manifest/package2.xml -u mycitestpartial --testlevel RunSpecifiedTests --runtests LWCUploadTPACtlTest
