24b3726e0ad04b0686ced9fdf9439d48a25cb00f
zhubj <810864669@qq.com>
2019-10-10 17:47:10 +0800

    deloyee

--- mycitest/deployee.bat
+++ mycitest/deployee.bat
@@ -1,14 +1,3 @@
 echo start
-@echo off
-
-set result=('"C:\Program Files\Salesforce CLI\bin\sfdx" force:source:deploy -x ./manifest/package2.xml -u mycitestpartial --testlevel RunSpecifiedTests --runtests LWCUploadTPACtlTest')
-
-for /f "delims=" %%i in (%result%) do (
-
-    set lastLine=%%~i
-
-)
-
-echo %lastLine%
-
-pause
+set deployPath=package2
+"C:\Program Files\Salesforce CLI\bin\sfdx" force:source:deploy -x ./manifest/%deployPath%.xml -u mycitestpartial --testlevel RunSpecifiedTests --runtests LWCUploadTPACtlTest
