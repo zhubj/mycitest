66527cb601038fc87eb0876ad5fd0b1e5bdbbe13
zhubj <810864669@qq.com>
2019-10-10 17:29:04 +0800

    deloyee

---
+++ mycitest/.history/deployee_20191010172714.bat
@@ -0,0 +1,3 @@
+echo ok
+cd ./mycitest
+"C:\Program Files\Salesforce CLI\bin\sfdx" force:source:deploy -x ./manifest/package2.xml -u mycitestpartial --testlevel RunSpecifiedTests --runtests LWCUploadTPACtlTest
