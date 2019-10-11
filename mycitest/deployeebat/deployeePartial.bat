5b3f87c62ca6b6db9458cab7be5241bac2fa34a8
zhubj <810864669@qq.com>
2019-10-11 17:07:48 +0800

    init

--- mycitest/deployeebat/deployeePartial.bat
+++ mycitest/deployeebat/deployeePartial.bat
@@ -1,6 +1,6 @@
 echo start
 
-for /f "tokens=1,2 delims==" %%i in (deployeesettings.txt) do (
+for /f "tokens=1,2 delims==" %%i in (./deployeebat/deployeesettings.txt) do (
 if "%%i"=="deployPath" set deployPath=%%j
 if "%%i"=="testclass" set testclass=%%j
 )
@@ -11,4 +11,4 @@ echo testclass:%testclass%
 echo %GIT_COMMIT%
 echo %GIT_BRANCH%
 echo %WORKSPACE%
-"C:\Program Files\Salesforce CLI\bin\sfdx" force:source:deploy -x ../manifest/%deployPath%.xml -u mycitestpartial --testlevel RunSpecifiedTests --runtests %testclass% 
+"C:\Program Files\Salesforce CLI\bin\sfdx" force:source:deploy -x ./manifest/%deployPath%.xml -u mycitestpartial --testlevel RunSpecifiedTests --runtests %testclass% 
